46baecf4d39ba5492410ed3caa414a48617c99c8
paulzpranay97 <paulpranay1997@gmail.com>
2022-12-01 22:45:12 +0530

    pranaycomplete

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,14 @@
 let num="1221";
 let bag="";
-for(let i=num.length-1;i>=0;i++)
+for(let i=num.length-1;i>=0;i--)
 {
 bag=bag+num[i];
 }
 if(bag==num)
 {
-  console.log("Yes palindrome");
+  console.log("Yes it is a palindrome");
 }
 else
 {
-  console.log("Not a palindrome");
+  console.log("No it is not a palindrome");
 }
--- primeNumber.js
+++ primeNumber.js
@@ -1,27 +1,10 @@
-let num=11;
+let num= 13;
 let count=0;
-if(num==1)
-{
-console.log("1 is neither prime nor composite")
-}
-else
-{
-if(num>1)
-{
-for(let i=1;i<=num;i++)
-{
-if(num%i===0)
-{
-count++;
-}
-}
-}
-if(count==2)
-{
-console.log("number is prime");
-}
-else
-{
-console.log("number is not prime");
-}
-}
+    for (let i=1; i<=num; i++){
+        if (num%i==0){
+            count++
+        }
+    }
+    if (count==2){console.log("Yes")}
+    else {console.log("No")}
+

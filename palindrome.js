836aa2b175c92b6ab5467f3a2b08d9a90d0f0bc0
Upasana raghav <119447299+Upasanaraghav01@users.noreply.github.com>
2022-12-01 21:42:36 +0530

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+let num="1221";
+let bag="";
+for(let i=num.length-1;i>=0;i++)
+{
+bag=bag+num[i];
+}
+if(bag==num)
+{
+  console.log("Yes palindrome");
+}
+else
+{
+  console.log("Not a palindrome");
+}
